c544da1d74303630afe17c7e4bcf5bf95def7407
Belka-S <belka.srg@gmail.com>
2023-05-03 08:25:09 +0200

    add hw-4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,56 +1,99 @@
-import { Component } from 'react';
+import { useReducer } from 'react';
 
 import { Options } from 'components/Options/Options';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Section } from 'components/Section/Section';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
+const reducer = (state, action) => ({ ...state, ...action });
+
+export const App = () => {
+  const [state, dispatch] = useReducer(reducer, {
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  addFeedback = option =>
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  const addFeedback = option => dispatch({ [option]: state[option] + 1 });
 
-  countTotalFeedback = () =>
-    Object.values(this.state).reduce((acc, el) => (acc += el));
+  const countTotalFeedback = () =>
+    Object.values(state).reduce((acc, el) => (acc += el));
 
-  countPositiveFeedbackPstg = () => this.state.good / this.countTotalFeedback();
+  const countPositiveFeedbackPstg = () => state.good / countTotalFeedback();
 
-  render = () => {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positivePstg = this.countPositiveFeedbackPstg();
+  const { good, neutral, bad } = state;
+  const totalFeedback = countTotalFeedback();
+  const positivePstg = countPositiveFeedbackPstg();
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <Options
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.addFeedback}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <Options options={Object.keys(state)} onLeaveFeedback={addFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {!totalFeedback ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePstg={positivePstg}
           />
-        </Section>
+        )}
+      </Section>
+    </>
+  );
+};
 
-        <Section title="Statistics">
-          {!totalFeedback ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePstg={positivePstg}
-            />
-          )}
-        </Section>
-      </>
-    );
-  };
-}
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   addFeedback = option =>
+//     this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+
+//   countTotalFeedback = () =>
+//     Object.values(this.state).reduce((acc, el) => (acc += el));
+
+//   countPositiveFeedbackPstg = () => this.state.good / this.countTotalFeedback();
+
+//   render = () => {
+//     const { good, neutral, bad } = this.state;
+//     const totalFeedback = this.countTotalFeedback();
+//     const positivePstg = this.countPositiveFeedbackPstg();
+
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <Options
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.addFeedback}
+//           />
+//         </Section>
+
+//         <Section title="Statistics">
+//           {!totalFeedback ? (
+//             <Notification message="There is no feedback" />
+//           ) : (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={totalFeedback}
+//               positivePstg={positivePstg}
+//             />
+//           )}
+//         </Section>
+//       </>
+//     );
+//   };
+// }
 
 /* {(() => {
   if (!totalFeedback)
